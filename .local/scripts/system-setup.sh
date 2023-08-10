751c34c814a2b10aa472169650b76ef9bccf931e
tjex <tjex@tjex.net>
2023-08-10 19:42:51 +0200

    system setup script

--- .local/scripts/system-setup.sh
+++ .local/scripts/system-setup.sh
@@ -1,20 +1,29 @@
 # script that installs all the various things I use (incase I need to reinstall a system from scratch)
 
-# clone down dotfiles first!
+# import ssh keys first
+
+# install homebrew, this should prompt to install xcode-terminal tools, which will include git
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# make needed directories
+mkdir -p ~/.local/share/zsh/plugins
+mkdir -p ~/.local/share/zsh/themes
+
+# setup dotfiles repo
+alias dotf='/usr/bin/git --git-dir=$HOME/.dotf-cfg/ --work-tree=$HOME'
+cd ~
+git clone --bare git@github.com:tjex/dotfiles.git $HOME/.dotf-cfg
+dotf checkout
+dotf config --local status.showUntrackedFiles no
 
 # zsh
+git clone https://github.com/sindresorhus/pure.git ~/.local/share/zsh/themes/pure
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ~/.local/share/zsh/plugins/fast-sytax-highlighting
 
-#git clone https://github.com/sindresorhus/pure.git "${ZDOTDIR}/pure" || exit
-#env CGO_ENABLED=0 go install -ldflags="-s -w" github.com/gokcehan/lf@latest
+# the restart shell so that all environment variables / pahths from dotfiles install are in scope
+exec zsh
 
-# arch
-
-declare -a packages=($(cat pacman-installed.txt | tr '\n' ' '))
-
-# for package in ${packages[@]}
-# do
-#     echo ${package}
-# done
-sudo pacman -Syu ${packages}
-
+# neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
