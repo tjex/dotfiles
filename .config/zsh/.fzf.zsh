4c83369f2687b058d7a5204f7cab6c0b81a33bd6
tjex <tjex@tjex.net>
2023-08-11 23:03:12 +0200

    add all configs with *

---
+++ .config/zsh/.fzf.zsh
@@ -0,0 +1,21 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+
+# FZF Config
+export FZF_DEFAULT_COMMAND='fd -H --type file' 
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border --border-label='╢ life is short, find it faster ╟' \
+                         --tabstop=4 --exact --ansi \
+                         --color gutter:-1,bg+:-1,fg+:yellow,hl:yellow,hl+:yellow,pointer:yellow"
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
