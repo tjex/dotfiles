cef024deea41f052f74bfda21eb1f68f4b0690f8
tjex <tjex@tjex.net>
2023-08-14 21:40:47 +0200

    tmux

--- .local/scripts/utility/tmux-init.sh
+++ .local/scripts/utility/tmux-init.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 tmux new-session -d -ssystem -c ~/.config
-tmux new-session -d -sgeneral -c ~/t-root
-tmux new-session -d -skth -c ~/t-root/ctech/kth
-tmux new-session -d -sobsidian -c ~/t-root/obsidian/paradigm-shifted
+tmux new-session -d -sgeneral -c ~/tjroot
+tmux new-session -d -sthesis -c ~/tjroot/ctech/kth
+tmux new-session -d -sobsidian -c ~/tjroot/obsidian/paradigm-shifted
 tmux a -tgeneral
