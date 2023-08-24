78d9af626541d67cc99685998a9644faa7517b26
tjex <tjex@tjex.net>
2023-08-24 15:49:09 +0200

    nvim colors

--- .config/nvim/lua/tjex/gui/init.lua
+++ .config/nvim/lua/tjex/gui/init.lua
@@ -1,5 +1,6 @@
 require('tjex.gui.statusline')
 require('tjex.gui.blankline')
 require('tjex.gui.devicons')
+-- require('tjex.gui.test')
 -- using own color scheme in after/plugin/
 -- require('tjex.gui.noirbuddy')
