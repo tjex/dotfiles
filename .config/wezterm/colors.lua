9c967a5d51306a80ba10a2a4c6a9852379637188
tjex <tjex@tjex.net>
2023-08-28 12:47:45 +0200

    colors

--- .config/wezterm/colors.lua
+++ .config/wezterm/colors.lua
@@ -11,7 +11,7 @@ local soft_red = '#936666'
 local purple = '#c3a0ba'
 local blue_1 = "#496c7a"
 local blue_2 = "#566981"
-local background = '#171A27'
+local background = "#161923"
 local foreground = '#949494'
 
 function module.apply(config)
