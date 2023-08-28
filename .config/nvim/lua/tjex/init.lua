d3b49658b1caf3cf4bca7e04961c9264661f7dac
tjex <tjex@tjex.net>
2023-08-28 06:27:50 +0200

    consistent file naming keymaps / keybinds

--- .config/nvim/lua/tjex/init.lua
+++ .config/nvim/lua/tjex/init.lua
@@ -1,7 +1,7 @@
 -- be aware of the sourcing order
 require('tjex.globals')
 require('tjex.vim-opt')
-require('tjex.keymaps')
+require('tjex.keybinds')
 require('tjex.funcs')
 require('tjex.commands')
 require('tjex.autocmds')
