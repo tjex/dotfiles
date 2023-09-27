65a363b5ee18a0901a61143d2a93c7b3d52566e9
tjex <tjex@tjex.net>
2023-09-27 22:55:39 +0200

    formatter.lua

---
+++ .config/nvim/after/plugin/formatter.lua
@@ -0,0 +1,29 @@
+local ok, _ = pcall(require, "formatter")
+if not ok then
+	print "formatter conf has a problem"
+	return
+end
+
+-- Utilities for creating configurations
+-- local util = require "formatter.util"
+
+-- Provides the Format, FormatWrite, FormatLock, and FormatWriteLock commands
+ require("formatter").setup {
+	logging = true,
+	log_level = vim.log.levels.WARN,
+	filetype = {
+		-- formatters are executed in order within their own tables
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+		},
+        -- TODO: shfmt isnt working via formatter.nvim
+		bash = {
+			require("formatter.filetypes.sh").shfmt,
+		},
+
+		-- for any filetype
+		["*"] = {
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
+}
