17b22aaf01083c4a795017afacd7e034457e6871
tjex <tjex@tjex.net>
2023-10-09 18:30:30 +0200

    shell functions

--- .config/zsh/.fzf.zsh
+++ .config/zsh/.fzf.zsh
@@ -13,8 +13,8 @@ fi
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
 # FZF Config
-export FZF_DEFAULT_COMMAND='fd -H -tf' # show hidden fiels and type="files" 
-export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border \
+export FZF_DEFAULT_COMMAND='fd -H -tf' # show hidden files and type="files" 
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border --multi --marker="☉-" \
                          --tabstop=4 --exact --ansi \
                          --color gutter:-1,bg+:-1,fg+:yellow,hl:yellow,hl+:yellow,pointer:yellow"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
