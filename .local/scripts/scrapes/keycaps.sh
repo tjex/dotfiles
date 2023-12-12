eeb34aca196bbb3c7c82a090ea8d6f6014b48304
tjex <tjex@tjex.net>
2023-12-12 21:45:45 +0100

    keycaps scrape edit for raspi

---
+++ .local/scripts/scrapes/keycaps.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env zsh
+
+if /usr/bin/curl "https://keygem.com/products/gazzew-u4tx-10pcs?_pos=5&_sid=c09c61649&_ss=r&variant=43756068995340" |\
+    /usr/bin/grep "Sold out</button>"; then
+    # html attribute found, still sold out
+else
+    /usr/bin/curl -d "gazzew u4tx RBG keycaps are back in stock. Product link: 
+    https://keygem.com/products/gazzew-u4tx-10pcs?_pos=5&_sid=c09c61649&_ss=r&variant=43756068995340" \
+    rpi.local/scrapes
+fi
+
