c3633cc2f97b660314395aa3c6c728db9f533df0
tjex <tjex@tjex.net>
2023-12-21 18:06:48 +0100

    zk config, lots of it

--- .config/nvim/after/plugin/zk.lua
+++ .config/nvim/after/plugin/zk.lua
@@ -4,31 +4,60 @@ if not ok then
 	return
 end
 
-local opts = { noremap = true, silent = true }
+local cmd = require("zk.commands")
 local key = vim.keymap.set
+local key_opts = { noremap = true, silent = true }
+local zk_root = require("zk.util").notebook_root(vim.fn.expand("%:p"))
+
 
 require("zk").setup({
-	-- can be "telescope", "fzf", "fzf_lua" or "select" (`vim.ui.select`)
-	-- it's recommended to use "telescope", "fzf" or "fzf_lua"
 	picker = "telescope",
 
+	-- lsp config is dealt with here instead of lsp.lua because .setup()
+	-- actually communicates directly with nvim-lsp to setup the server.
+	-- trying to integrate it in mason-lsp will probably end badly.
 	lsp = {
-		-- `config` is passed to `vim.lsp.start_client(config)`
 		config = {
 			cmd = { "zk", "lsp" },
 			name = "zk",
 			on_attach = function()
-                require("cmp_nvim_lsp").default_capabilities()
+				require("cmp_nvim_lsp").default_capabilities()
 				key("n", "gd", function()
 					vim.lsp.buf.definition()
-				end, opts)
+				end, key_opts)
 
-                key("n", "<leader>zn", ":ZkNew {title = vim.fn.input('Title: '), dir = vim.fn.input('Dir: ')}<cr>")
-                key("v", "<leader>zil", ":ZkInsertLinkAtSelection")
-                key("v", "<leader>zm", ":ZkMatch")
-                key("v", "<leader>zt", ":ZkTags")
+				-- normal mode
+				key(
+					"n",
+					"<leader>zn",
+					":ZkNew {title = vim.fn.input('Title: '), dir = vim.fn.input('Dir: '), template = vim.fn.input('Template: ')}<cr>"
+				)
+                -- daily notes
+				key("n", "<leader>zd", function()
+					local dir = "d/" .. vim.fn.strftime("%Y")
+					local title = vim.fn.strftime("%Y-%M-D")
+					print(zk_root, dir)
+					cmd.get("ZkNew")({ dir = dir, title = title, group = "daily", template = "daily.md" })
+				end)
+				key("n", "<leader>zt", ":ZkTags<cr>")
+				key("n", "<leader>zf", ":ZkNotes<cr>")
+				key("n", "<leader>zl", ":ZkInsertLink {title = vim.fn.input('Link text: ')}<cr>")
+				key("n", "<leader>zo", ":ZkLinks<cr>")
+				key("n", "<leader>zb", ":ZkBackLinks<cr>")
 
-
+				-- visual mode
+				key(
+					"v",
+					"<leader>zl",
+					":ZkInsertLinkAtSelection {matchSelected = vim.fn.input('look for matches? ')}<cr>"
+				)
+				key("v", "<leader>zm", ":ZkMatch<cr>")
+				key("v", "<leader>zn", ":ZkNewFromTitleSelection {dir = vim.fn.input('Dir: ')}<cr>")
+				key(
+					"v",
+					"<leader>ze",
+					":ZkNewFromContentSelection {title = vim.fn.input('Title: '), dir = vim.fn.input('Dir: ')}<cr>"
+				)
 			end,
 		},
 
