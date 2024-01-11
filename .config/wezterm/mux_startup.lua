91b9dbad7812765945046eac183041e11fa89105
tjex <tjex@tjex.net>
2024-01-11 16:45:40 +0100

    wez: mux conf

--- .config/wezterm/mux_startup.lua
+++ .config/wezterm/mux_startup.lua
@@ -5,58 +5,60 @@ local M = {}
 function M.wide_screen()
 	wezterm.on("mux-startup", function()
 		-- dev
-		local a1_tab, _, a1_window = mux.spawn_window({
+		local dev_tab, _, dev_window = mux.spawn_window({
 			workspace = "dev",
 			cwd = "/Users/tjex/.config",
 		})
-		a1_tab:set_title("config")
+		dev_tab:set_title("config")
 
-		local a2_tab, _, _ = a1_window:spawn_tab({
+		local zkorg_tab, _, _ = dev_window:spawn_tab({
 			cwd = "/Users/tjex/.local/src/zk-org/",
 		})
 
-		a2_tab:set_title("zk-org")
+		zkorg_tab:set_title("zk-org")
 
 		-- admin
-		local b1_tab, b1_pane, b1_window = mux.spawn_window({
+		local admin_tab, admin_pane, admin_window = mux.spawn_window({
 			workspace = "admin",
-			cwd = "/Users/tjex/docs/logs",
+			cwd = "/Users/tjex/docs/",
 		})
-		b1_tab:set_title("admin")
-        b1_pane:send_text("nvim .\n")
+		admin_tab:set_title("admin")
 
-		local neomutt = b1_pane:split({direction = "Right"})
-        neomutt:send_text("TERM=xterm-direct neomutt\n")
+		local neomutt = admin_pane:split({ direction = "Right" })
+		neomutt:send_text("TERM=xterm-direct neomutt\n")
 
-        local lf = b1_pane:split({direction = "Top", cwd = "/Users/tjex/docs/"})
-        lf:send_text("lf\n")
+		local lf = admin_pane:split({ direction = "Top" })
+		lf:send_text("lf\n")
 
-        neomutt:activate()
+		neomutt:activate()
 
-		local b3_tab, _, _ = b1_window:spawn_tab({
+		local music_tab, _, _ = admin_window:spawn_tab({
 			cwd = "/Users/tjex/music",
 		})
-		b3_tab:set_title("music")
+		music_tab:set_title("music")
+
+		local rss_tab = admin_window:spawn_tab({args = {"newsboat"}})
+        rss_tab:set_title("rss")
 
 		-- writing
-		local c1_tab, c1_pane, _ = mux.spawn_window({
+		local writing_tab, writing_pane, _ = mux.spawn_window({
 			workspace = "writing",
 			cwd = "/Users/tjex/wikis/ps",
 		})
-		c1_tab:set_title("zk - ps")
-		c1_pane:send_text("zk open\n")
+		writing_tab:set_title("zk - ps")
+		writing_pane:send_text("zk open\n")
 
 		-- website
-		local d1_tab, d1_pane, d1_window = mux.spawn_window({
+		local website_tab, website_pane, website_window = mux.spawn_window({
 			workspace = "website",
 			cwd = "/Users/tjex/dev/websites/tjex.net",
 		})
-		d1_pane:send_text("yarn run start\n")
-		d1_tab:set_title("node")
+		website_pane:send_text("yarn run start\n")
+		website_tab:set_title("node")
 
-		local d2_tab, d2_pane = d1_window:spawn_tab({})
-		d2_tab:set_title("tjex.net")
-		d2_pane:split({ size = 0.3, direction = "Left", cwd = "/Users/tjex/dev/websites/pico/scss/" })
+		local astro_tab, astro_pane = website_window:spawn_tab({})
+		astro_tab:set_title("tjex.net")
+		astro_pane:split({ size = 0.3, direction = "Left", cwd = "/Users/tjex/dev/websites/pico/scss/" })
 	end)
 end
 
