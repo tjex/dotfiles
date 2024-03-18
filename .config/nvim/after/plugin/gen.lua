e3a64b4120851ad31d25aeef4abbe4be63f0a806
tjex <tjex@tjex.net>
2024-03-18 20:53:44 +1100

    nvim: gen config

--- .config/nvim/after/plugin/gen.lua
+++ .config/nvim/after/plugin/gen.lua
@@ -9,24 +9,46 @@ local prompt = require("gen").prompts
 
 require("gen").setup({
 	model = "mixtral", -- The default model to use.
+	host = "localhost", -- The host running the Ollama service.
+	port = "11434", -- The port on which the Ollama service is listening.
 	display_mode = "split", -- The display mode. Can be "float" or "split".
-	show_prompt = false, -- Shows the Prompt submitted to Ollama.
+	show_prompt = true, -- Shows the Prompt submitted to Ollama.
 	show_model = true, -- Displays which model you are using at the beginning of your chat session.
+	quit_map = "q", -- set keymap for quit
 	no_auto_close = true, -- Never closes the window automatically.
 	init = function()
 		pcall(io.popen, "ollama serve > /dev/null 2>&1 &")
 	end,
-	-- Function to initialize Ollama
-	command = "curl --silent --no-buffer -X POST http://localhost:11434/api/generate -d $body",
-	-- The command for the Ollama service. You can use placeholders $prompt, $model and $body (shellescaped).
-	-- This can also be a lua function returning a command string, with options as the input parameter.
-	-- The executed command must return a JSON object with { response, context }
-	-- (context property is optional).
-
-	debug = false, -- Prints errors and the command which is run.
+    -- Function to initialize Ollama
+	command = function(options)
+		return "curl --silent --no-buffer -X POST http://"
+			.. options.host
+			.. ":"
+			.. options.port
+			.. "/api/chat -d $body"
+	end,
+	debug = true, -- Prints errors and the command which is run.
 })
 
-prompt["test_prompt"] = {
-	prompt = "test",
+prompt["explain_code"] = {
+	prompt = "Explain how the following code works, then give some examples of what problems the code could help solve. \nHere is the code:\n\n```\n$text\n```",
 	replace = false,
 }
+
+prompt["optimize_code"] = {
+	prompt = "Optimize the following code, and explain concisely why the changes were made:\n ```$filetype\n$text\n```",
+	replace = true,
+    extract = "```$filetype\n(.-)```",
+}
+
+prompt["improve_writing"] = {
+	prompt = "Improve the following text by making it clearer and easier to understand: $text",
+	replace = true,
+}
+
+prompt["ask"] = {
+	prompt = "$input",
+	replace = false,
+}
+
+
