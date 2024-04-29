d1cf9e93caf122131e530e3aab31a7bdfaf6c3eb
tjex <tjex@tjex.net>
2024-04-29 15:44:24 +1000

    nvim statusline tweaks

--- .config/nvim/after/plugin/statusline.lua
+++ .config/nvim/after/plugin/statusline.lua
@@ -1,24 +1,15 @@
 local function status_line()
-	local right_align = "%="
-	local file_type = " %y "
-	local file_name = " %-.45f "
+	local center = "%=%="
+	local file_type = "%y"
+	local file_name = "%-.45f "
 
-	local buf_nr = "[%n]:"
-	local modified = "%-m"
-	local line_no = "%4((%l/%L%))"
-	local pct_thru_file = "%4p%%"
-	local fugitive = " %{FugitiveStatusline()} "
+	local buf_nr = "• %n • "
+	local modified = "%-m "
+	local line_no = "%4((%l/%L%)) "
+	local pct_thru_file = "%4p%% "
+	local fugitive = "%{FugitiveStatusline()} "
 
-	return string.format(
-		"%s%s%s%s%s%s%s",
-		right_align,
-		file_type,
-		buf_nr,
-		file_name,
-		line_no,
-		fugitive,
-		right_align
-	)
+	return string.format("%s%s%s%s%s%s%s", center, modified, file_name, buf_nr, line_no, fugitive, center)
 end
 
 vim.opt.statusline = status_line()
