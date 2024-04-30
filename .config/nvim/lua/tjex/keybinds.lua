36b56d5629e27eebe1eedf7c9ac1c3fb217b35bd
tjex <tjex@tjex.net>
2024-04-30 16:04:38 +1000

    convert keybinds.lua to use tjex.keymap

--- .config/nvim/lua/tjex/keybinds.lua
+++ .config/nvim/lua/tjex/keybinds.lua
@@ -1,17 +1,16 @@
 -- neovim keypings
 local key = require("tjex.keymap")
-local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- main functionality remaps
 vim.api.nvim_set_keymap("", "<Space>", "<Nop>", { noremap = true, silent = true })
-key("n", "<ESC>", "<Nop>")
-key("n", "<F1>", "<Nop>", opts)
-key("i", "kj", "<ESC>", opts)
-key("i", "KJ", "<ESC>", opts)
-key("i", "<c-l>", "<Nop>", opts)
+key.nmap({ "<ESC>", "<Nop>" })
+key.nmap({ "<F1>", "<Nop>" })
+key.imap({ "kj", "<ESC>" })
+key.imap({ "KJ", "<ESC>" })
+key.imap({ "<c-l>", "<Nop>" })
 
 -- Modes
 --  normal mode = 'n'
@@ -22,49 +21,49 @@ key("i", "<c-l>", "<Nop>", opts)
 --  command mode = 'c'
 
 -- file operations
-key("n", "W", ":w<CR>", opts)
-key("n", "<leader>cd", ":cd %:h<CR>", opts)
+key.nmap({ "W", ":w<CR>" })
+key.nmap({ "<leader>cd", ":cd %:h<CR>" })
 
 -- text operations
-key("n", "˚", "mzJ`z", opts) -- append below to current line and leave cursor where it is
-key("x", "<leader>p", '"_d<Left>p', opts) -- paste over text and keep same item in paste buffer
-key("n", "<leader>y", '"+y', opts) -- copy to sys clipboard
-key("v", "<leader>y", '"+y', opts)
-key("n", "x", '"_x', opts) -- stop 'x' from adding to paste
-key("n", "Z", "1z=e", opts)
+key.nmap({ "˚", "mzJ`z" }) -- append below to current line and leave cursor where it is
+key.xmap({ "<leader>p", '"_d<Left>p' }) -- paste over text and keep same item in paste buffer
+key.nmap({ "<leader>y", '"+y' }) -- copy to sys clipboard
+key.vmap({ "<leader>y", '"+y' })
+key.nmap({ "x", '"_x' }) -- stop 'x' from adding to paste
+key.nmap({ "Z", "1z=e" })
 
 -- Replace mode using the word you were currently on (thanks to the Primeageon).
-key("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/<Left>")
+key.nmap({ "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/<Left>" })
 
 -- naviagtion
-key("n", "j", "gj", opts) -- cursor down inline
-key("n", "k", "gk", opts) -- cursor up inline
-key("v", "J", ":m '>+1<CR>gv=gv", opts) -- move selected visual lines down
-key("v", "K", ":m '<-2<CR>gv=gv", opts) -- move selected visual lines up
-key("n", "<C-d>", "<C-d>zz", opts) -- keep cursor in middle when jumping
-key("n", "<C-u>", "<C-u>zz", opts) -- keep cursor in middle when jumping
-key("n", "n", "nzzzv", opts) -- keep cursor in middle during search
-key("n", "N", "Nzzzv", opts)
-key("n", "`o", '`"', opts) -- jump to where cursor was on file close (`" sucks to type)
+key.nmap({ "j", "gj" }) -- cursor down inline
+key.nmap({ "k", "gk" }) -- cursor up inline
+key.vmap({ "J", ":m '>+1<CR>gv=gv" }) -- move selected visual lines down
+key.vmap({ "K", ":m '<-2<CR>gv=gv" }) -- move selected visual lines up
+key.nmap({ "<C-d>", "<C-d>zz" }) -- keep cursor in middle when jumping
+key.nmap({ "<C-u>", "<C-u>zz" }) -- keep cursor in middle when jumping
+key.nmap({ "n", "nzzzv" }) -- keep cursor in middle during search
+key.nmap({ "N", "Nzzzv" })
+key.nmap({ "`o", '`"' }) -- jump to where cursor was on file close (`" sucks to type)
 
 -- windows
-key("n", "<C-h>", "<C-w>h", opts)
-key("n", "<C-j>", "<C-w>j", opts)
-key("n", "<C-k>", "<C-w>k", opts)
-key("n", "<C-l>", "<C-w>l", opts)
+key.nmap({ "<C-h>", "<C-w>h" })
+key.nmap({ "<C-j>", "<C-w>j" })
+key.nmap({ "<C-k>", "<C-w>k" })
+key.nmap({ "<C-l>", "<C-w>l" })
 
 -- tabs
-key("n", "gh", ":tabp<CR>", opts) -- previous tab
-key("n", "gl", ":tabn<CR>", opts) -- next tab
+key.nmap({ "gh", ":tabp<CR>" }) -- previous tab
+key.nmap({ "gl", ":tabn<CR>" }) -- next tab
 
 -- buffers
-key("n", "<leader>bd", ":bn<bar>sp<bar>bp<bar>bd<CR>")
+key.nmap({ "<leader>bd", ":bn<bar>sp<bar>bp<bar>bd<CR>" })
 
 -- plugins without their own conf file
 
 -- easy-align
-key("n", "ga", ":EasyAlign", opts)
-key("v", "ga", ":EasyAlign", opts)
+key.nmap({ "ga", ":EasyAlign" })
+key.vmap({ "ga", ":EasyAlign" })
 
 -- centerpad
-key("n", "<c-ESC>", ":Centerpad 20<cr>", opts)
+key.nmap({ "<c-ESC>", ":Centerpad 20<cr>" })
