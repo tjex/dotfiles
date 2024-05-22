8594b0a15aed8bc79ed3c69b099ae4b08a1f8023
tjex <tjex@tjex.net>
2024-05-22 20:13:34 +1000

    nvim: add markdown-preview plugin

--- .config/nvim/after/plugin/packer.lua
+++ .config/nvim/after/plugin/packer.lua
@@ -61,6 +61,16 @@ return require("packer").startup(function(use)
 		"junegunn/vim-easy-align",
 	})
 	use({
+		"iamcco/markdown-preview.nvim",
+		run = "cd app && yarn install",
+		cmd = "MarkdownPreview",
+		setup = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	})
+
+	use({
 		"ten3roberts/bookmarks.nvim", --using a fork for now until the main project merges its pull request
 		branch = "feat-scoped-bookmarks",
 	})
