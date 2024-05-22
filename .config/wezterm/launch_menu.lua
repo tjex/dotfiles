819a4c775cf0ac8646d14b726a1267158e8136a0
tjex <tjex@tjex.net>
2024-05-22 20:12:32 +1000

    wezterm: launcer env paths

--- .config/wezterm/launch_menu.lua
+++ .config/wezterm/launch_menu.lua
@@ -1,12 +1,23 @@
 local M = {}
 
+local paths = {
+	"/bin:",
+	"/usr/bin:",
+	"/usr/local/bin:",
+	"/opt/homebrew/bin:",
+	"/Users/tjex/.local/go/bin:",
+	"/Users/tjex/.local/share/nvm/versions/node/v20.5.1/bin",
+}
+
+local paths_string = table.concat(paths)
+
 function M.apply(config)
 	config.launch_menu = {
 		{
 			label = "tech wiki",
 			cwd = "/Users/tjex/wikis/tech/",
 			set_environment_variables = {
-				PATH = "/bin:/usr/bin:/usr/local/bin:/opt/homebrew/bin:/Users/tjex/.local/go/bin",
+				PATH = paths_string,
 			},
 			args = { "zsh", "-c", "nvim $(fd -E .zk -t f | fzf)" },
 		},
@@ -14,7 +25,7 @@ function M.apply(config)
 			label = "edit nvim conf",
 			cwd = "/Users/tjex/.config/nvim",
 			set_environment_variables = {
-				PATH = "/bin:/usr/bin:/usr/local/bin:/opt/homebrew/bin",
+				PATH = paths_string,
 			},
 			args = { "zsh", "-c", "nvim $(fd -E 'bookmarks' | fzf)" },
 		},
@@ -22,7 +33,7 @@ function M.apply(config)
 			label = "edit shell scripts",
 			cwd = "/Users/tjex/.scripts",
 			set_environment_variables = {
-				PATH = "/bin:/usr/bin:/usr/local/bin:/opt/homebrew/bin",
+				PATH = paths_string,
 			},
 			args = { "zsh", "-c", "nvim $(fd | fzf)" },
 		},
@@ -30,7 +41,7 @@ function M.apply(config)
 			label = "edit navi files",
 			cwd = "/Usrs/tjex/.local/share/navi",
 			set_environment_variables = {
-				PATH = "/bin:/usr/bin:/usr/local/bin:/opt/homebrew/bin",
+				PATH = paths_string,
 			},
 			-- for some reason, this command needs to be explicityly cd'd into?
 			args = { "zsh", "-c", "cd /Users/tjex/.local/share/navi && nvim $(fd | fzf)" },
