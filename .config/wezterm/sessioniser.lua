e3e967ea5359ac40bd0c8f8607030ca83dbd7b7c
tjex <tjex@tjex.net>
2024-07-21 08:47:55 +0200

    wez: sessioniser tweaks

--- .config/wezterm/sessioniser.lua
+++ .config/wezterm/sessioniser.lua
@@ -9,11 +9,13 @@ local dev = "/Users/tjex/dev"
 
 M.toggle = function(window, pane)
 	local projects = {}
+	local home = os.getenv("HOME") .. "/"
 
 	local success, stdout, stderr = wezterm.run_child_process({
 		fd,
 		"-HI",
 		"-td",
+		"--max-depth=1",
 		"--prune",
 		".",
 		lsrc,
@@ -27,33 +29,29 @@ M.toggle = function(window, pane)
 		return
 	end
 
+	-- fill table with results
 	for line in stdout:gmatch("([^\n]*)\n?") do
 		local project = line
-		local label = project
-		local id = project:sub(1, -2)
-		id = id:gsub(".*/", "")
+		local label = project:gsub(home, "")
+		local _, _, id = string.find(project, ".*/(.+)/")
 
 		-- handle git bare repositories,
 		-- assuming following name convention `myproject.git`
 		if string.match(project, "%.git/$") then
-			-- wezterm.log_info("found .git " .. tostring(project))
 			success, stdout, stderr =
 				wezterm.run_child_process({ fd, "-HI", "-td", "--max-depth=1", ".", project .. "worktrees" })
 			if success then
 				for wt_line in stdout:gmatch("([^\n]*)\n?") do
 					local wt_project = wt_line
-					local wt_label = wt_project
+					local wt_label = wt_project:gsub(home, "")
 					local wt_id = wt_project
-					if string.find(wt_line, "worktrees") then
-						table.insert(projects, { label = tostring(wt_label), id = tostring(wt_id) })
-					end
+					table.insert(projects, { label = tostring(wt_label), id = tostring(wt_id) })
 				end
 			else
-				wezterm.log_error("Failed to run fd: " .. stderr)
+				wezterm.log_error("Failed to run fd for git worktree: " .. stderr)
 			end
 		end
 
-		local display_label = label:gsub(os.getenv("HOME") .. "/", "")
 		table.insert(projects, { label = tostring(label), id = tostring(id) })
 	end
 
@@ -64,7 +62,13 @@ M.toggle = function(window, pane)
 					wezterm.log_info("Cancelled")
 				else
 					wezterm.log_info("Selected " .. label)
-					win:perform_action(act.SwitchToWorkspace({ name = id, spawn = { cwd = label } }), pane)
+					win:perform_action(
+						act.SwitchToWorkspace({
+							name = id,
+							spawn = { cwd = home .. label },
+						}),
+						pane
+					)
 				end
 			end),
 			fuzzy = true,
