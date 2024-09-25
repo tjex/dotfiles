42a6b7cd64be22bfc2e4875ab2b354550afde7a7
tjex <tjex@tjex.net>
2024-09-25 19:34:08 +0200

    batch update

--- .config/zsh/.fzf.zsh
+++ .config/zsh/.fzf.zsh
@@ -14,10 +14,11 @@ source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
 # FZF Config
 export FZF_DEFAULT_COMMAND='fd -H -tf' # show hidden files and type="files" 
-export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border --multi --marker='+' \
+export FZF_DEFAULT_OPTS="--height 40% --layout=reverse --border --multi --marker='|' \
                          --tabstop=4 --exact --ansi \
-                         --color gutter:-1,bg+:blue,fg+:black,hl:red,hl+:black,pointer:black"
+                         --color gutter:-1,bg+:-1,fg+:white,hl:white,hl+:white,pointer:blue,border:blue"
 export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 export BKMR_FZF_OPTS="--reverse --height 40%"
+
